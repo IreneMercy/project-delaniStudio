ee81e0dca3931fa8f70e08c5ce15ba78c1accc5c
IreneMercy <irenemercy700@gmail.com>
2019-07-19 21:35:23 +0300

    add hover on mouse enter to portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,71 @@
 $(document).ready(function(){
   // Design
   $(".design").click(function(){
-    $(".design").slideToggle();
+    $(".design").hide().toggle();
     $("#design").slideToggle();
   });
 
-  $("#design").click(function(){
-    $(".design").slideToggle();
-    $("#design").slideToggle();
-  });
+
   // development
   $(".development").click(function(){
-    $(".development").slideToggle();
+    $(".development").hide().toggle();
     $("#development").slideToggle();
   });
-  $("#development").click(function(){
-    $("#development").slideToggle();
-    $(".development").slideToggle();
-  });
+
   // Product management
   $(".products").click(function(){
-    $(".products").slideToggle();
+    $(".products").hide().toggle();
     $("#products").slideToggle();
   });
-  $("#products").click(function(){
-    $("#products").slideToggle();
-    $(".products").slideToggle();
+
+
+  // work4box
+  $(".work4").mouseenter(function(){
+    $("#work4box").show().mouseleave(function(){
+      $("#work4box").hide();
+    });
   });
+
+  $(".work2").mouseenter(function(){
+    $("#work2box").show().mouseleave(function(){
+      $("#work2box").hide();
+    });
+  });
+
+  $(".work3").mouseenter(function(){
+    $("#work3box").show().mouseleave(function(){
+      $("#work3box").hide();
+    });
+  });
+
+  $(".work1").mouseover(function(){
+    $("#work1box").show().mouseleave(function(){
+      $("#work1box").hide();
+    });
+  });
+
+  $(".work5").mouseover(function(){
+    $("#work5box").show().mouseleave(function(){
+      $("#work5box").hide();
+    });
+  });
+
+  $(".work6").mouseover(function(){
+    $("#work6box").show().mouseleave(function(){
+      $("#work6box").hide();
+    });
+  });
+
+  $(".work7").mouseover(function(){
+    $("#work7box").show().mouseleave(function(){
+      $("#work7box").hide();
+    });
+  });
+
+  $(".work8").mouseover(function(){
+    $("#work8box").show().mouseleave(function(){
+      $("#work8box").hide();
+    });
+  });
+
 });
